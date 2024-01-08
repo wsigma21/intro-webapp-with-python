c42e1dca7fe90815f2f72180257c82043f735ed2
wsigma21 <wallstlbl@gmail.com>
2024-01-08 13:51:38 +0900

    refactor: テンプレートファイルのディレクトリ名をsettingsで設定

--- chapter19/settings.py
+++ chapter19/settings.py
@@ -2,5 +2,9 @@ import os
 
  # 実行ファイルのあるディレクトリの指定
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # 静的配信するファイルを置くディレクトリ
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# テンプレートファイルを置くディレクトリ
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
