932a22162019be425ecdae38131380cc12229be5
wsigma21 <wallstlbl@gmail.com>
2024-01-08 14:13:32 +0900

    add: chapter20 first commit

---
+++ chapter20/views.py
@@ -0,0 +1,51 @@
+import textwrap
+import urllib.parse
+from datetime import datetime
+from pprint import pformat
+
+from henango.http.request import HTTPRequest
+from henango.http.response import HTTPResponse
+from henango.template.renderer import render
+
+def now(request: HTTPRequest) -> HTTPResponse:
+    """
+    現在時刻を表示するHTMLを生成する
+    """
+    context = {"now": datetime.now()}
+    body = render("now.html", context)
+
+    return HTTPResponse(body=body)
+
+
+def show_request(request: HTTPRequest) -> HTTPResponse:
+    """
+    HTTPリクエストの内容を表示するHTMLを生成する
+    """
+    context = {"request": request, "headers": pformat(request.request_header), body: request.body.decode("utf-8", "ignore")}
+    body = render("show_request.html", context)
+    return HTTPResponse(body=body)
+
+def parameters(request: HTTPRequest) -> HTTPResponse:
+    """
+    POSTパラメータを表示するHTMLを表示する
+    """
+
+    if request.method == "GET":
+        body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
+
+        return HTTPResponse(body=body, status_code = 405)
+    
+    elif request.method == "POST":
+        context = {"params": urllib.parse.parse_qs(request.body.decode())}
+        body = render("parameters.html", context)
+
+        return HTTPResponse(body=body)
+
+def user_profile(request: HTTPRequest) -> HTTPResponse:
+    """
+    ユーザのプロフィールを表示するHTMLを生成する
+    """
+    context = {"user_id": request.params["user_id"]}
+    body = render("user_profile.html", context)
+
+    return HTTPResponse(body=body)
